e179799413e68113b7e1112d8bef68426a93691e
DeividSkii <darlandeivid64@gmail.com>
2026-08-14 23:06:28 -0300

    Adição das classes peça, servico e categoriapeça

---
+++ src/main/java/models/Peca.java
@@ -0,0 +1,91 @@
+package models;
+
+import java.time.LocalDateTime;
+
+public class Peca {
+
+    private long id;
+    private String nome;
+    private String codigoDeBarras;
+    private String fornecedor;
+    private int qtdEstoque;
+    private double precoCusto;
+    private double precoVenda;
+    private LocalDateTime dataCadastro;
+    private LocalDateTime dataUltimaAtualizacao;
+    private CategoriaPeca categoriaPeca;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setFornecedor(String fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+
+    public void setQtdEstoque(int qtdEstoque) {
+        this.qtdEstoque = qtdEstoque;
+    }
+
+    public void setPrecoCusto(double precoCusto) {
+        this.precoCusto = precoCusto;
+    }
+
+    public void setPrecoVenda(double precoVenda) {
+        this.precoVenda = precoVenda;
+    }
+
+    public void setCategoriaPeca(CategoriaPeca categoriaPeca) {
+        this.categoriaPeca = categoriaPeca;
+    }
+
+    public LocalDateTime getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCodigoDeBarras() {
+        return codigoDeBarras;
+    }
+
+    public String getFornecedor() {
+        return fornecedor;
+    }
+
+    public int getQtdEstoque() {
+        return qtdEstoque;
+    }
+
+    public double getPrecoCusto() {
+        return precoCusto;
+    }
+
+    public double getPrecoVenda() {
+        return precoVenda;
+    }
+
+    public LocalDateTime getDataUltimaAtualizacao() {
+        return dataUltimaAtualizacao;
+    }
+
+    public CategoriaPeca getCategoriaPeca() {
+        return categoriaPeca;
+    }
+
+    public Peca(long id, String nome, String codigoDeBarras, String fornecedor, double precoVenda, double precoCusto, CategoriaPeca categoriaPeca) {
+        this.id = id;
+        this.nome = nome;
+        this.codigoDeBarras = codigoDeBarras;
+        this.fornecedor = fornecedor;
+        this.precoVenda = precoVenda;
+        this.precoCusto = precoCusto;
+        this.categoriaPeca = categoriaPeca;
+    }
+}
